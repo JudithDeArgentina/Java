a7bd5766f42bcfa36bed726024a808d2baf85946
Alejandra CAC <alejandra.j.gomez@gmail.com>
2022-06-26 23:15:53 -0300

    Mi primer programa en Java - Hola MUndo

---
+++ holamundo.java
@@ -0,0 +1,8 @@
+class Hola{
+     public static void main(String[] argumentos){
+        System.out.println("Hola mundo");
+       
+           System.out.println("Esto es una prueba a ver si falla");
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+class Main{
+    
+    public static void main(String[] argumentos){
+        System.out.println("Hola mundo");
+        System.out.println("Como estas?");
+        // Este es mi primer programa en java
+    }
+}
